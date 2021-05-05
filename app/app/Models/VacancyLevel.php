60ccf00963d5ee67745f8b873fc9be8296beafaa
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2021-05-05 20:54:33 +0900

    Techpit1章すべて終わらせた

---
+++ app/app/Models/VacancyLevel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VacancyLevel extends Model
+{
+    
+    private $remainingCount;
+
+    public function __construct(int $remainingCount) {
+        $this->remainingCount = $remainingCount;
+    }
+
+    public function __toString(): string
+    {
+        return $this->mark();
+    }
+    
+    public function mark():string
+    {
+         if ($this->remainingCount === 0) {
+            return '×';
+        }
+        if ($this->remainingCount < 5) {
+            return '△';
+        }
+        return '◎';
+    }
+
+    public function slug():string
+    {
+        if ($this->remainingCount === 0) {
+            return 'empty';
+        }
+        if ($this->remainingCount < 5) {
+            return 'few';
+        }
+        return 'enough';
+    }
+}
---
+++ app/tests/Unit/Models/VacancyLevelTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use PHPUnit\Framework\TestCase;
+use App\Models\VacancyLevel;
+
+class VacancyLevelTest extends TestCase
+{    
+    /**
+     * @param int $remainingCount
+     * @param string $expectedMark
+     * @dataProvider dataMark
+     * @return void
+     */
+    public function testMark(int $remainingCount, string $expectedMark)
+    {
+        $level = new VacancyLevel($remainingCount);
+        $this->assertSame($expectedMark, $level->mark());
+    }
+    
+    /**
+     * testSlug
+     *
+     * @param int $remainingCount
+     * @param string $expectedSlug
+     * @dataProvider dataSlug
+     * @return void
+     */
+    public function testSlug(int $remainingCount, string $expectedSlug)
+    {
+        $level = new VacancyLevel($remainingCount);
+        $this->assertSame($expectedSlug, $level->slug());
+    }
+
+    public function dataMark()
+    {
+        return [
+            '空きなし' => [
+                'remainingCount' => 0,
+                'expectedMark' => '×',
+            ],
+            '残りわずか' => [
+                'remainingCount' => 4,
+                'expectedMark' => '△',
+            ],
+            '空き十分' => [
+                'remainingCount' => 5,
+                'expectedMark' => '◎',
+            ],
+        ];
+    }
+
+    public function dataSlug()
+    {
+        return [
+            '空きなし' => [
+                'remainingCount' => 0,
+                'expectedMark' => 'empty',
+            ],
+            '残りわずか' => [
+                'remainingCount' => 4,
+                'expectedMark' => 'few',
+            ],
+            '空き十分' => [
+                'remainingCount' => 5,
+                'expectedMark' => 'enough',
+            ],
+        ];
+    }
+}
