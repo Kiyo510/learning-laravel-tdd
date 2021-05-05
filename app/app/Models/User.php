b73aca33a02620e2a1aad6d78a4e47c3e3a02355
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2021-05-06 00:52:41 +0900

    Mokeryの学習をした

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Lesson;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -36,4 +39,29 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function reservations(): HasMany
+    {
+        $this->hasMany(Reservation::class);
+    }
+
+    public function canReserve(Lesson $lesson): bool
+    {
+        if ($lesson->remainingCount() === 0) {
+            return false;
+        }
+        if ($this->plan === 'gold') {
+            return true;
+        }
+        return $this->reservationCountThisMonth() < 5;
+    }
+    
+    public function reservationCountThisMonth(): int
+    {
+        $today = Carbon::today();
+        return $this->reservations()
+            ->whereYear('created_at', $today->year)
+            ->whereMonth('created_at', $today->month)
+            ->count();
+    }
 }
--- app/tests/Unit/Models/UserTest.php
+++ app/tests/Unit/Models/UserTest.php
@@ -2,17 +2,70 @@
 
 namespace Tests\Unit\Models;
 
+use App\Models\User;
+use App\Models\Lesson;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
+
     /**
-     * A basic unit test example.
+     * testCanReserve
      *
-     * @return void
+     * @param  string $plan
+     * @param  int $remainingCount
+     * @param  int $reservationCount
+     * @param  bool $canReserve
+     * @dataProvider dataCanReserve
      */
-    public function testExample()
+    public function testCanReserve(string $plan, int $remainingCount, int $reservationCount, bool $canReserve)
     {
-        $this->assertTrue(true);
+        /** @var User $user */
+        $user = Mockery::mock(User::class)->makePartial();
+        $user->shouldReceive('reservationCountThisMonth')->andReturn($reservationCount);
+        $user->plan = $plan;
+
+        /** @var Lesson $lesson */
+        $lesson = Mockery::mock(Lesson::class);
+        $lesson->shouldReceive('remainingCount')->andReturn($remainingCount);
+
+        $this->assertSame($canReserve, $user->canReserve($lesson, $reservationCount));
+    }
+
+    public function dataCanReserve()
+    {
+        return [
+            '予約可能枠数>1, レギュラー, 当月予約<5, 予約可' => [
+                'plan' => 'regular',
+                'remainingCount' => 1,
+                'reservationCount' => 4,
+                'canReserve' => true,
+            ],
+            '予約可能枠数>1, レギュラー, 当月予約>5, 予約不可' => [
+                'plan' => 'regular',
+                'remainingCount' => 1,
+                'reservationCount' => 5,
+                'canReserve' => false,
+            ],
+            '予約可能枠数0, レギュラー, 当月予約<5, 予約不可' => [
+                'plan' => 'regular',
+                'remainingCount' => 0,
+                'reservationCount' => 4,
+                'canReserve' => false,
+            ],
+            '予約可能枠数>1, ゴールド, 当月予約無制限, 予約可' => [
+                'plan' => 'gold',
+                'remainingCount' => 1,
+                'reservationCount' => 5,
+                'canReserve' => true,
+            ],
+            '予約可能枠数0, ゴールド, 当月予約無制限, 予約不可' => [
+                'plan' => 'gold',
+                'remainingCount' => 0,
+                'reservationCount' => 5,
+                'canReserve' => false,
+            ],
+        ];
     }
 }
